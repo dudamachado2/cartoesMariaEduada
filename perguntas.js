db54260808c3efd603dc82630751d0116b7842b3
dudamachado2 <machado.silva.maria0601@escola.pr.gov.br>
2025-06-30 10:37:20 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Pergunta',
+    'Qual a capital de Santa Catarina?',
+    'Florianópolis'
+)
+
+criaCartao(
+    'Pergunta',
+    'Quantos dias tem um ano?',
+    '365 dias'
+)
+
+criaCartao(
+    'Pergunta',
+    'Quantas horas tem 3 dias?',
+    '72 horas'
+)
